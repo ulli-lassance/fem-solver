27315a1dc59c1f7f33b2fe91bf6729a53026fad6
ulli-lassance <john.lassance@gmail.com>
2026-06-25 19:26:59 -0300

    comments

--- examples/transmission_line.py
+++ examples/transmission_line.py
@@ -2,11 +2,15 @@ from easy_builder import EasyGeom
 
 def generate_geometry(mesh_size):
     builder = EasyGeom("transmission_line")
-    
-    builder.set_air_domain(0, 0, 0.4, 0.1)
 
-    builder.add_circle(0.1,0.05,0.025, 1000)
+    # define the outer box
+    builder.set_air_domain(0, 0, 0.4, 0.1, boundary_voltage=0.0)
 
-    builder.add_circle(0.3,0.05,0.025, -1000)
+    # left conductor (1000V)
+    builder.add_circle(0.1,0.05,0.025, voltage=1000)
+
+    # right conductor (-1000V)
+    builder.add_circle(0.3,0.05,0.025, voltage=-1000)
     
+    # generates the finite element mesh
     builder.build_and_mesh(mesh_size)
